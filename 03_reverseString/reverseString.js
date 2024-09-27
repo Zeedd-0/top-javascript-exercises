f863664db1836df7b0535529a90fa006289d8a4f
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-09-27 13:16:38 -0300

    Create function to clear exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const spelled = string.split('');
+    const reversedSpelled = [];
+    for (const letter of spelled) {
+        reversedSpelled.unshift(letter);
+    }
+    return reversedSpelled.join('');
 };
 
 // Do not edit below this line
