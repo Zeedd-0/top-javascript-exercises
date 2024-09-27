2e57cf22d2f3081d482c658cb7a4f7015d701063
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-09-27 11:45:00 -0300

    Update with solution comparison and comments

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -10,6 +10,18 @@ const repeatString = function(text, number) {
     return result;
 };
 
+// solution is clean and better:
+// once everything is in a function, can use return to break and return a value
+// even the variables have a better name
+const solution = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+      string += word;
+    }
+    return string;
+};
+
 
 // Do not edit below this line
 module.exports = repeatString;
