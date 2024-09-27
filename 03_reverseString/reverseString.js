3afe30e6ee013aed32fb21ea301a93091f5ecf19
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-09-27 13:18:57 -0300

    Add solution, comparison and comments

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -7,5 +7,14 @@ const reverseString = function(string) {
     return reversedSpelled.join('');
 };
 
+
+// solution used .reverse
+// and got it all in one line
+// more readable and clean
+const reverseString = function (string) {
+    return string.split("").reverse().join("");
+};
+
+
 // Do not edit below this line
 module.exports = reverseString;
