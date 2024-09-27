1b94e91c0d6d11c21c37d5cacf306d9d970c6e9a
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-09-27 11:38:22 -0300

    Update functions to clear exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    let result = '';
+    if (number >= 0) {
+        for (let i = 0; i < number; i++){
+            result += text;
+        };
+    } else {
+        result += 'ERROR';
+    };
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
