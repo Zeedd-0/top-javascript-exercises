f070e5d6ab0258ba9046eaca44161ae61f7d3c3d
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-09-30 16:00:46 -0300

    Update with solution for comparison

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -7,7 +7,24 @@ const leapYears = function(year) {
 };
 
 
-
+// solution given is better beacuse it makes a single
+// if rule against two of mine
+// i got close to a one rule solution, but couldn't see
+// the negative (!) making the rule work, so i made two rules
+const solution = function (year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+  
+    if (
+      isYearDivisibleByFour &&
+      (!isCentury || isYearDivisibleByFourHundred)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
 
 // Do not edit below this line
