9306227bd88b3eb4e4faf756633d29b822ee38da
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-09-30 16:10:55 -0300

    Update solution for comparison

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return ((temp - 32) * (5/9)).toFixed(1) * 1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    return (temp * (9/5) + 32).toFixed(1) * 1;
+};
+
+
+// solution is different, don't know if is better
+// it uses Math.round()
+// i used .toFixed()
+const solutionToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+};
+
+const solutionToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
